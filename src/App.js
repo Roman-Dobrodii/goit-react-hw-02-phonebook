9040a2322573c4fbf031d66a146c81071be23de4
Roman Dobrodii <romariosi007@gmail.com>
2021-01-13 21:18:28 +0200

    Подготавливает приложение

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Phonebook from '../src/components/Phonebook';
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      <Phonebook />
+    </>
+  );
+};
+
+export default App;
